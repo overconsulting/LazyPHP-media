b0611320802a02cdf2dd3409071e9088c23c849e
ldandoy <ldandoy@overconsulting.net>
2017-04-06 13:57:17 +0200

    Mise à jour

--- Media/assets/js/media.js
+++ Media/assets/js/media.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 
 function mediaTypeChange(event) {
 	var type = $("select[name=type]")[0];
-	$("#formMedia input[type=file].media").parents(".form-group").hide();
-	$("#formMedia input[type=file].media.media-"+type.options[type.selectedIndex].value).parents(".form-group").show();
+	if (type != null) {
+		$("#formMedia input[type=file].media").parents(".form-group").hide();
+		$("#formMedia input[type=file].media.media-"+type.options[type.selectedIndex].value).parents(".form-group").show();
+	}
 }
